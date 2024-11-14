7395e09c579923d005d5d6b5a29004de50fa30b7
Jakob Pistohl <gh@jp-bits.de>
2024-11-14 17:48:25 +0100

    Remove ble_device __parse()

--- ble_device.py
+++ ble_device.py
@@ -4,90 +4,58 @@ from manufacturers import Manufacturer
 import log
 
 class BleDevice:
-    manufacturer = Manufacturer()
-
-    device = None
-    details = None
-    props = None
-
-    name = None
-    name2 = None
-    address = None
-    address2 = None
-    addresstype = None
-    alias = None
-    paired = None
-    bonded = None
-    trusted = None
-    blocked = None
-    servicedata = None,
-    advertisingflags = None,
-    legacypairing = None
-    rssi = None
-    connected = None
-    uuids = None
-    manufacturers = None
-    manufacturer_binary = None
-    txpower = None
-    servicesresolved = None
-    class_name = None
-    modalias = None
-    icon = None
-
-    timestamp = str(datetime.datetime.now().replace(microsecond=0)) # timestamp in seconds
-    geolocation = None # TODO current pc location (GPS/text input?) (if available)
 
     def __init__(self, device):
-        self.device = device
+        self.manufacturer = Manufacturer()
+
         self.details = device.details
         self.props = self.details['props']
-        self.__parse()
 
-    def __parse(self):
-        if self.device and self.details and self.props:
-            self.address = self.device.address
-            self.name = self.device.name
+        self.name = device.name
+        self.name2 = self.__in_props("Name")
+        self.address = device.address
+        self.address2 = self.__in_props("Address")
+        self.addresstype = self.__in_props("AddressType")
+        self.alias = self.__in_props("Alias")
+        self.paired = self.__in_props("Paired")
+        self.bonded = self.__in_props("Bonded")
+        self.trusted = self.__in_props("Trusted")
+        self.blocked = self.__in_props("Blocked")
+        self.servicedata = self.__in_props("ServiceData"),
+        if self.servicedata is not None:
+            self.servicedata = str(self.servicedata)
+        self.advertisingflags = self.__in_props("AdvertisingFlags"),
+        if self.advertisingflags is not None:
+            self.advertisingflags = str(self.advertisingflags)
+        self.legacypairing = self.__in_props("LegacyPairing")
+        self.rssi = self.__in_props("RSSI")
+        self.connected = self.__in_props("Connected")
+        self.uuids = self.__in_props("UUIDs")
+        if self.uuids is not None:
+            self.uuids = ",".join(self.uuids)
+        manu_data = self.__in_props("ManufacturerData")
+        if manu_data is not None:
+            self.manufacturers = ",".join([str(s) for s in self.manufacturer.parse(manu_data)])
+            self.manufacturer_binary = ",".join([b.hex() for b in list(manu_data.values())])
+        self.txpower = self.__in_props("TxPower")
+        self.servicesresolved = self.__in_props("ServicesResolved")
+        self.class_name = self.__in_props("Class")
+        self.modalias = self.__in_props("Modalias")
+        self.icon = self.__in_props("Icon")
+        self.timestamp = str(datetime.datetime.now().replace(microsecond=0)) # timestamp in seconds
+        self.geolocation = None # TODO current pc location (GPS/text input?) (if available)
 
-            self.name2 = self.__in_props("Name")
-            self.address2 = self.__in_props("Address")
-            self.addresstype = self.__in_props("AddressType")
-            self.alias = self.__in_props("Alias")
-            self.paired = self.__in_props("Paired")
-            self.bonded = self.__in_props("Bonded")
-            self.trusted = self.__in_props("Trusted")
-            self.blocked = self.__in_props("Blocked")
-            self.servicedata = self.__in_props("ServiceData")
-            if self.servicedata is not None:
-                self.servicedata = str(self.servicedata)
-            self.advertisingflags = self.__in_props("AdvertisingFlags")
-            if self.advertisingflags is not None:
-                self.advertisingflags = str(self.advertisingflags)
-            self.legacypairing = self.__in_props("LegacyPairing")
-            self.rssi = self.__in_props("RSSI")
-            self.connected = self.__in_props("Connected")
-            self.uuids = self.__in_props("UUIDs")
-            if self.uuids is not None:
-                self.uuids = ",".join(self.uuids)
-            self.class_name = self.__in_props("Class")
-            self.modalias = self.__in_props("Modalias")
-            self.icon = self.__in_props("Icon")
+        self.props = self.details['props']
 
-            manu_data = self.__in_props("ManufacturerData")
-            if manu_data is not None:
-                self.manufacturers = ",".join([str(s) for s in self.manufacturer.parse(manu_data)])
-                self.manufacturer_binary = ",".join([b.hex() for b in list(manu_data.values())])
-            self.txpower = self.__in_props("TxPower")
-            self.servicesresolved = self.__in_props("ServicesResolved")
+        done_props = "Class", "Modalias", "Icon", "Name", "Address", "AddressType", "Alias", "Paired", "Bonded", "Trusted", "Blocked", "LegacyPairing", "RSSI", "Connected", "UUIDs", "ManufacturerData", "ServiceData", "AdvertisingFlags", "TxPower", "ServicesResolved", "Adapter"
 
-            done_props = "Class", "Modalias", "Icon", "Name", "Address", "AddressType", "Alias", "Paired", "Bonded", "Trusted", "Blocked", "LegacyPairing", "RSSI", "Connected", "UUIDs", "ManufacturerData", "ServiceData", "AdvertisingFlags", "TxPower", "ServicesResolved", "Adapter"
-
-            missing_props = [p for p in self.props if p not in done_props]
-            if missing_props:
-                log.warning(f"MISSED PROPS({self.address}): {missing_props}.")
-            if self.name != None and self.name2 != None and self.name != self.name2:
-                log.warning(f"Names don't match: {self.name} - {self.name2}")
-            if self.address != None and self.address2 != None and self.address != self.address2:
-                log.warning(f"Addresses don't match: {self.address} - {self.address2}")
+        missing_props = [p for p in self.props if p not in done_props]
+        if missing_props:
+            log.warning(f"MISSED PROPS({self.address}): {missing_props}.")
+        if self.name != None and self.name2 != None and self.name != self.name2:
+            log.warning(f"Names don't match: {self.name} - {self.name2}")
+        if self.address != None and self.address2 != None and self.address != self.address2:
+            log.warning(f"Addresses don't match: {self.address} - {self.address2}")
 
     def __in_props(self, search):
         if self.props != None and search in self.props:
