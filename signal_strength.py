64f390fa9530cefb34ae20da0c847931f827e2f0
Jakob Pistohl <gh@jp-bits.de>
2025-01-23 00:21:08 +0100

    Improve signal_strength options output

--- signal_strength.py
+++ signal_strength.py
@@ -10,9 +10,6 @@ import log
 import ble_device
 from TUI import TUITable, GUIGraph
 
-# TODO rename
-dev = []
-
 # TODO maybe rewrite original?
 class BleScanner:
     callback = None
@@ -66,7 +63,6 @@ class BleScanner:
 def addr_callback(device: BLEDevice, advertisement_data: AdvertisementData):
     if device.address != search_address:
         return
-    # dev.append(device)
 
     tx_power = advertisement_data.tx_power
     rssi = advertisement_data.rssi
@@ -85,8 +81,37 @@ def addr_callback(device: BLEDevice, advertisement_data: AdvertisementData):
     print("")
 
 def tui_callback(device: BLEDevice, advertisement_data: AdvertisementData):
+    devs.append(ble_device.BleDevice(device))
     table.update(ble_device.BleDevice(device))
 
+devs = []
+
+# get all devices with low RSSI (and there timeline)
+import numpy as np
+def get_timings(devs):
+    # devs = [d for d in devs if d.rssi > -60]
+    timestamps = [datetime.strptime(d.timestamp, '%Y-%m-%d %H:%M:%S') for d in devs]
+    diffs = np.array([(timestamps[i+1] - timestamps[i]).total_seconds() for i in range(len(timestamps) - 1)])
+    print(f"{diffs.size} discoveries have an avg of {diffs.mean()}")
+    return diffs
+
+# {d.name for d in dev_m}
+
+def print_options(data):
+    ind_width = len(str(len(data)))
+    name_width = max(len(d['name']) for d in data)
+    type_width = max(len(d['device_type']) for d in data)
+    manu_width = max(len(d['manufacturer']) for d in data)
+
+    row_format = f"{{:<{ind_width}}}  {{:<{name_width}}}  {{:<{type_width}}}  {{:<{manu_width}}}"
+
+    for i, option in enumerate(data):
+        print(row_format.format(
+            i + 1,
+            option['name'],
+            option['device_type'],
+            option['manufacturer']
+        ))
 
 if __name__ == "__main__":
     search_address = None
@@ -101,8 +126,7 @@ if __name__ == "__main__":
     options = table.get_sorted_data()
     if options:
         while True:
-            for i, option in enumerate(options):
-                print(f"{i + 1}:\t{option['name']}\t{option['device_type']}\t{option['manufacturer']}")
+            print_options(options)
 
             choice = input("index: ")
             try:
